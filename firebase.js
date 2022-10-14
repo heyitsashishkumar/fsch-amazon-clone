5ddd026d8c3d275f8b927e541953a4f503922c20
ashish1553 <ashish765082@gmail.com>
2022-10-14 06:44:54 +0530

    checkout page and next-auth added

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyChUWv97J-VEgIx_6_MislCaskAGrlN89Q",
+    authDomain: "ak-verse.firebaseapp.com",
+    projectId: "ak-verse",
+    storageBucket: "ak-verse.appspot.com",
+    messagingSenderId: "321920042812",
+    appId: "1:321920042812:web:9f8be0da5bfefb42864039"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import Image from 'next/image'
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/router'
+import { route } from 'next/dist/next-server/server/router';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
 
 const Header = () => {
+    const { data: session } = useSession();
+
+    const router = useRouter();
+
+    const items = useSelector(selectItems);
+
     return (
         <header>
             {/* Top Nav */}
             <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
                 {/* Company logo */}
                 <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-                    <Image src="https://links.papareact.com/f90" objectFit='contain' className='cursor-pointer' width={150} height={40} />
+                    <Image onClick={() => router.push('/')} src="https://links.papareact.com/f90" objectFit='contain' className='cursor-pointer' width={150} height={40} />
                 </div>
 
                 {/* Search */}
@@ -17,16 +28,16 @@ const Header = () => {
                     <MagnifyingGlassIcon className='h-12 p-4' />
                 </div>
                 <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-                    <div className='link'>
-                        <p>Hello Ashish Kumar</p>
+                    <div onClick={!session ? signIn : signOut} className='link'>
+                        <p>{session ? `Hello, ${session?.user?.name}` : 'Sign In'}</p>
                         <p className='font-extrabold md:text-sm'>Account & List</p>
                     </div>
                     <div className='link'>
                         <p>Returns</p>
                         <p className='font-extrabold md:text-sm'>& Orders</p>
                     </div>
-                    <div className='relative link flex items-center'>
-                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
+                    <div className='relative link flex items-center' onClick={() => router.push('/checkout')}>
+                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>{items.length}</span>
                         <ShoppingCartIcon className='h-10' />
                         <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
                     </div>
