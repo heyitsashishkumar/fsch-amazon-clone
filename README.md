4905be1ece351d1089f0f08d81bae5c194a7c049
ashish1553 <ashish765082@gmail.com>
2022-10-16 13:02:30 +0530

    log added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Deployed at: https://amazon-ak-build.vercel.app/
 
+https://amazon-ak-build-6k9m6kqs6-ashish1553.vercel.app
+
 
 
 Docs:
--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -34,6 +34,7 @@ export default async (req, res) => {
         try {
             event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
         } catch (err) {
+            console.log(err);
             console.log('ERROR', err.message);
             return res.status(400).send(`Webhook Error: ${err.message}`);
         }
